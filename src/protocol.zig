18fa2f4a8ce3178d1053df9fcd1e03867d9dd1c7
Omniscient <omniscient.oce@gmail.com>
2023-05-25 22:44:57 +1000

    add the comments back in

--- src/protocol.zig
+++ src/protocol.zig
@@ -1,3 +1,7 @@
+// This module defines the Zena protocol
+//
+// The protocol is a request/response protocol
+// with a custom wire format (i.e. not using HTTP)
 const std = @import("std");
 
 pub const MsgType = enum {
@@ -5,12 +9,24 @@ pub const MsgType = enum {
     SendMsg,
 };
 
-pub const Message = struct { id: u32, author: u32 };
+pub const Message = struct {
+    /// This is the unique ID for the message IN the relevant
+    /// channel. IDs across channels are not unique
+    id: u32,
+    /// author ID - this is unique across a single server instance.
+    /// (multi-server/federation is a non-goal)
+    author: u32,
+    // /// message contents string
+    // FIXME: contents: []const u8,
+};
 
 // takes a Message and returns a slice of bytes to send over the wire
 // pub fn serialise_message(msg: Message) []const u8 {
 //     _ = msg;
 // }
+
+// string encoding
+// | len(u32) | data |
 
 /// takes a slice of bytes and returns a Message
 pub fn deserialise_message(buffer: []const u8) Message {
