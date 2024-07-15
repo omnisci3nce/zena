1c587128893191b0ca56e85d4a278bf229e2eb2d
omnisci3nce <omniscient.oce@gmail.com>
2024-07-15 21:49:46 +1000

    playing with gpui

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -6,14 +6,28 @@ use gpui::*;
 
 struct HelloWorld {
     text: SharedString,
+    count: usize
+}
+
+actions!(client, [
+    IncrCount
+]);
+
+impl HelloWorld {
+    fn onclick(&mut self, event: &MouseDownEvent, cx: &mut ViewContext<Self>) {
+        self.count += 1;
+        cx.notify();
+        ()
+    }
 }
 
 impl Render for HelloWorld {
-    fn render(&mut self, _cx: &mut ViewContext<Self>) -> impl IntoElement {
+    fn render(&mut self, cx: &mut ViewContext<Self>) -> impl IntoElement {
         div()
             .flex()
             .bg(rgb(0x2e7d32))
-            .size(Length::Definite(Pixels(300.0).into()))
+            .size(Length::Definite(DefiniteLength::Fraction(1.0)))
+            // .size(Length::Definite(Pixels(300.0).into()))
             .justify_center()
             .items_center()
             .shadow_lg()
@@ -21,7 +35,10 @@ impl Render for HelloWorld {
             .border_color(rgb(0x0000ff))
             .text_xl()
             .text_color(rgb(0xffffff))
-            .child(format!("Hello, {}!", &self.text))
+            .on_mouse_down(MouseButton::Left, cx.listener(Self::onclick))
+            .child(
+                format!("Count: {}!", &self.count)
+            )
     }
 }
 
@@ -30,12 +47,13 @@ fn main() {
         let bounds = Bounds::centered(None, size(px(300.0), px(300.0)), cx);
         cx.open_window(
             WindowOptions {
-                window_bounds: Some(WindowBounds::Windowed(bounds)),
+                // window_bounds: Some(WindowBounds::Windowed(bounds)),
                 ..Default::default()
             },
             |cx| {
                 cx.new_view(|_cx| HelloWorld {
-                    text: "World".into(),
+                    text: "from Zena".into(),
+                    count: 0,
                 })
             },
         )
--- src/client/components/channel_list.rs
+++ src/client/components/channel_list.rs
@@ -1,13 +1,13 @@
-use egui::{RichText, Ui};
+// use egui::{RichText, Ui};
 
-use crate::model::Channel;
+// use crate::model::Channel;
 
-pub fn draw_channel_list(ui: &mut Ui, channels: &[Channel], selected_channel: Option<usize>) {
-    channels.iter().enumerate().for_each(|(idx, ch)| {
-        let text = format!("Channel {}: {}", ch.id, ch.name);
-        match selected_channel {
-            Some(i) if i == idx => ui.label(RichText::new(text).strong()),
-            _ => ui.label(text),
-        };
-    });
-}
+// pub fn draw_channel_list(ui: &mut Ui, channels: &[Channel], selected_channel: Option<usize>) {
+//     channels.iter().enumerate().for_each(|(idx, ch)| {
+//         let text = format!("Channel {}: {}", ch.id, ch.name);
+//         match selected_channel {
+//             Some(i) if i == idx => ui.label(RichText::new(text).strong()),
+//             _ => ui.label(text),
+//         };
+//     });
+// }
