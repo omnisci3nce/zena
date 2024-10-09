bd739db027c7459a815348c441998e0e479ba874
omniscient <17525998+omnisci3nce@users.noreply.github.com>
2024-10-09 21:59:33 +1100

    send username back with messages

--- server/messaging_service.go
+++ server/messaging_service.go
@@ -43,10 +43,18 @@ func (m *MessengingService) SendNewMessage(ctx context.Context, msg *proto.NewMe
 		return nil, status.Errorf(codes.Internal, "Failed to get last insert ID: %v", err)
 	}
 
+	var username string
+	row := m.db.QueryRow("SELECT username FROM users WHERE user_id = ?", &msg.AuthorId)
+	err = row.Scan(&username)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "Failed to get author's username: %v", err)
+	}
+
 	return &proto.Message{
-		MsgId:   string(id),
-		Content: msg.Content,
-		Author:  msg.AuthorId,
+		MsgId:      string(id),
+		Content:    msg.Content,
+		AuthorId:   msg.AuthorId,
+		AuthorName: username,
 		// Timestamp: time.Now().Unix(),
 	}, nil
 }
@@ -64,7 +72,7 @@ func (m *MessengingService) GetChannels(_req *proto.Empty, stream grpc.ServerStr
 }
 
 func (m *MessengingService) GetMessages(_req *proto.Empty, stream grpc.ServerStreamingServer[proto.Message]) error {
-	rows, err := m.db.Query("SELECT msg_id, contents, author_id FROM messages")
+	rows, err := m.db.Query("SELECT msg_id, contents, author_id, users.username FROM messages INNER JOIN users ON users.user_id = author_id")
 	if err != nil {
 		return status.Errorf(codes.Internal, "Failed to query messages: %v", err)
 	}
@@ -72,7 +80,7 @@ func (m *MessengingService) GetMessages(_req *proto.Empty, stream grpc.ServerStr
 
 	for rows.Next() {
 		var msg proto.Message
-		if err := rows.Scan(&msg.MsgId, &msg.Content, &msg.Author); err != nil {
+		if err := rows.Scan(&msg.MsgId, &msg.Content, &msg.AuthorId, &msg.AuthorName); err != nil {
 			return status.Errorf(codes.Internal, "Failed to scan channel row: %v", err)
 		}
 		stream.Send(&msg)
